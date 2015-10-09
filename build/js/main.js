5227acc2f45c92c85220205b912c6eec15651310
Yauhen Aliakseyenko <yauheni.aliakseyenko@neklo.com>
2015-10-09 22:42:48 +0300

    update

---
+++ build/js/main.js
@@ -0,0 +1,52 @@
+$(function(){
+
+  var carusel = $('#slider-objects-list');
+  var arrowPrevObj = $('#arrow-prev-obj');
+  var arrowNextObj = $('#arrow-next-obj');
+
+  var owlCarusel = carusel.owlCarousel({
+    autoPlay: 3000, //Set AutoPlay to 3 seconds
+    items : 4,
+    dragBeforeAnimFinish: false,
+    mouseDrag: false,
+    touchDrag: false,
+    pagination: false,
+    navigation: false
+  }).data('owlCarousel');
+
+  arrowPrevObj.on('click', function(e){
+    e.preventDefault();
+    owlCarusel.prev();
+  });
+
+  arrowNextObj.on('click', function(e){
+    e.preventDefault();
+    owlCarusel.next();
+  });
+
+  var sliderHeader = $('#slider-header');
+  var arrowPrevHeader = $('#arrow-prev-header');
+  var arrowNextHeader = $('#arrow-next-header');
+
+  var owlHeader = sliderHeader.owlCarousel({
+    dragBeforeAnimFinish: false,
+    mouseDrag: false,
+    touchDrag: false,
+    pagination: false,
+    navigation: false,
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem:true
+  }).data('owlCarousel');
+
+  arrowPrevHeader.on('click', function(e){
+    e.preventDefault();
+    owlHeader.prev();
+  });
+
+  arrowNextHeader.on('click', function(e){
+    e.preventDefault();
+    owlHeader.next();
+  });
+
+});
