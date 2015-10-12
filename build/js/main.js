3c9a42a2e3147680a05c5b75e4ce79d08ee1d517
ealexseen <ealexsee@gmail.com>
2015-10-12 20:10:43 +0300

    fix

--- build/js/main.js
+++ build/js/main.js
@@ -1,62 +1,61 @@
 $(function(){
 
-  var carusel = $('#slider-objects-list');
-  var arrowPrevObj = $('#arrow-prev-obj');
-  var arrowNextObj = $('#arrow-next-obj');
+    var carusel = $('#slider-objects-list');
+    var arrowPrevObj = $('#arrow-prev-obj');
+    var arrowNextObj = $('#arrow-next-obj');
 
-  var owlCarusel = carusel.owlCarousel({
-    autoPlay: 10000, 
-    items : 4,
-    dragBeforeAnimFinish: false,
-    mouseDrag: false,
-    touchDrag: false,
-    pagination: false,
-    navigation: false
-  }).data('owlCarousel');
+    var owlCarusel = carusel.owlCarousel({
+        autoPlay: 10000,
+        items : 4,
+        dragBeforeAnimFinish: false,
+        mouseDrag: false,
+        touchDrag: false,
+        pagination: false,
+        navigation: false
+    }).data('owlCarousel');
 
-  arrowPrevObj.on('click', function(e){
-    e.preventDefault();
-    owlCarusel.prev();
-  });
+    arrowPrevObj.on('click', function(e){
+        e.preventDefault();
+        owlCarusel.prev();
+    });
 
-  arrowNextObj.on('click', function(e){
-    e.preventDefault();
-    owlCarusel.next();
-  });
+    arrowNextObj.on('click', function(e){
+        e.preventDefault();
+        owlCarusel.next();
+    });
 
-  var sliderHeader = $('#slider-header');
-  var arrowPrevHeader = $('#arrow-prev-header');
-  var arrowNextHeader = $('#arrow-next-header');
+    var sliderHeader = $('#slider-header');
+    var arrowPrevHeader = $('#arrow-prev-header');
+    var arrowNextHeader = $('#arrow-next-header');
 
-  var owlHeader = sliderHeader.owlCarousel({
-      autoPlay: 10000,
-      dragBeforeAnimFinish: false,
-    mouseDrag: false,
-    touchDrag: false,
-    pagination: false,
-    navigation: false,
-    slideSpeed : 300,
-    paginationSpeed : 400,
-    singleItem:true
-  }).data('owlCarousel');
+    var owlHeader = sliderHeader.owlCarousel({
+        autoPlay: 10000,
+        dragBeforeAnimFinish: false,
+        mouseDrag: false,
+        touchDrag: false,
+        pagination: false,
+        navigation: false,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+    }).data('owlCarousel');
 
-  arrowPrevHeader.on('click', function(e){
-    e.preventDefault();
-    owlHeader.prev();
-  });
+    arrowPrevHeader.on('click', function(e){
+        e.preventDefault();
+        owlHeader.prev();
+    });
 
-  arrowNextHeader.on('click', function(e){
-    e.preventDefault();
-    owlHeader.next();
-  });
+    arrowNextHeader.on('click', function(e){
+        e.preventDefault();
+        owlHeader.next();
+    });
 
+    var calculator = $('#calculator_new');
+    var calculatorBtn = $('#calc_open_and_close');
 
-    $('.good-page-tocart-block').each(function(){
-        var self = $(this);
-        var html = self.find('*');
-        if (!html.length){
-            self.hide();
-        }
+    calculatorBtn.on('click', function(e){
+        e.preventDefault();
+        calculator.toggle();
     });
 
 });
